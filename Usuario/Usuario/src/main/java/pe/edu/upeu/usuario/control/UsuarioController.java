080aac3caf668d301525dfd5f35297641199db90
Juan Benique <juanbenique170@gmail.com>
2025-09-19 02:38:13 -0500

    menu establecido

--- Usuario/Usuario/src/main/java/pe/edu/upeu/usuario/control/UsuarioController.java
+++ Usuario/Usuario/src/main/java/pe/edu/upeu/usuario/control/UsuarioController.java
@@ -1,4 +1,21 @@
 package pe.edu.upeu.usuario.control;
 
+
+import javafx.fxml.FXML;
+import org.springframework.stereotype.Controller;
+import org.w3c.dom.Text;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Controller
 public class UsuarioController {
+    @FXML private TextField txtNum1, txtNum2;
+    @FXML private Button btningresado;
+    @FXML private Label ingresado;
+
+    @FXML
+    private void  ingresado(){
+
+    }
 }
